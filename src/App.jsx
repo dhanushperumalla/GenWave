1b84c144e0bbdb1492fed7a64bb3205d13cae86f
Perumalla Venkata Naga Dhansush <perumalladhanush102@gmail.com>
2024-05-13 11:08:43 +0530

    Benfits Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -9,7 +10,8 @@ const App = () => {
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Hero></Hero>
+        <Hero />
+        <Benefits />
       </div>
       <ButtonGradient />
     </>
